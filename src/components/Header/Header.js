af8377cbf9fa17a43dccc40694dddaf0efd55985
Januarius-Njoku <76001800+Januarius-Njoku@users.noreply.github.com>
2022-03-13 02:04:32 +0100

    Building the site with NextJS and React frameworks

---
+++ src/components/Header/Header.js
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import React from "react";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+  AiFillTwitterSquare,
+} from "react-icons/ai";
+import { DiCssdeck } from "react-icons/di";
+
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+  Span,
+} from "./HeaderStyles";
+
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "red",
+            marginBottom: "20px",
+          }}
+        >
+          <DiCssdeck size="3rem" />
+          <Span>Portfolio</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/cjaynjoku">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://linkedin.com/cjaynjoku">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://twitter.com/cjaynjoku">
+        <AiFillTwitterCircle size="3rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
+);
+
+export default Header;
